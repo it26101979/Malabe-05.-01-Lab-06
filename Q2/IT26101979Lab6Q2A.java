4a41f8f3e676b91583bfbc61cbbe73abc8549db4
it26101979 <it26101979@my.sliit.lk>
2026-09-16 09:38:03 +0530

    Add files via upload

---
+++ Q2/IT26101979Lab6Q2A.java
@@ -0,0 +1,15 @@
+
+public class IT26101979Lab6Q2A {
+    public static void main(String[] args) {
+       
+		
+		for(int x=1; x<=10; x++ ){
+			if (x==10){
+				System.out.print(x);
+			}else{
+				System.out.print(x+", ");
+			}
+		}
+		
+	}
+}
